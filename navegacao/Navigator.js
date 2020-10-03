dcbbb880974a971c77c5b38329d5cfeb53cf4b10
silas742 <66025250+silas742@users.noreply.github.com>
2020-10-02 23:27:21 -0300

    Update Navigator.js

--- navegacao/Navigator.js
+++ navegacao/Navigator.js
@@ -2,14 +2,14 @@ import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from 'react-navigation';
 import Cores from "../constantes/Cores";
-import DetalhesDoLugarTela from "../telas/DetalhesDoLugarTela";
-import ListaDeLugaresTela from "../telas/ListaDeLugaresTela";
+import DetalhesDoAmbienteTela from "../telas/DetalhesDoAmbienteTela";
+import ListaLugaresNaTela from "../telas/ListaLugaresNaTela";
 import MapaTela from "../telas/MapaTela";
 import NovoLugarTela from "../telas/NovoLugarTela";
 
 const Navigator = createStackNavigator ({
-  ListaDeLugares: ListaDeLugaresTela,
-  DetalhesDoLugar: DetalhesDoLugarTela,
+  ListaLugaresNaTela: ListaLugaresNaTela,
+  DetalhesDoAmbienteTela: DetalhesDoAmbienteTela,
   NovoLugar: NovoLugarTela,
   Mapa: MapaTela
 }, {
